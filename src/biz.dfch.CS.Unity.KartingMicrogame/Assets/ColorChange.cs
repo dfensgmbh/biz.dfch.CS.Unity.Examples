74d7ef05a6357c07c2b9f1c50be6c3785616aee7
FynnKaeser <fynn.kaeser@icloud.com>
2020-11-18 17:11:51 +0100

    Adding C# script to change color on collision with ramp

---
+++ src/biz.dfch.CS.Unity.KartingMicrogame/Assets/ColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        }*/
+    }
+}
