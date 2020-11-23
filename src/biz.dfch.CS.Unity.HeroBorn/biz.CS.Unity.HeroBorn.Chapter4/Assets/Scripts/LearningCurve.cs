76103528cdc1aa36e33a6465ca52d6cffa7dca3d
FynnKaeser <fynn.kaeser@icloud.com>
2020-11-23 09:59:44 +0100

    Creating Subfolders for different Chapters in book

---
+++ src/biz.dfch.CS.Unity.HeroBorn/biz.CS.Unity.HeroBorn.Chapter4/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+namespace Assets.Scripts
+
+{
+    public class LearningCurve : MonoBehaviour
+    {
+        private int currentAge = 30;
+        public int AddedAge = 1;
+
+        public float Pi = 3.14f;
+        public string WelcomeMessage = "Welcome to my Unity Game";
+        public bool IsMarked = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ComputeAge();
+            var isGenerated = GenerateCharacter("Yasuo");
+        }
+
+        /// <summary>
+        /// Computes a modified age integer
+        /// </summary>
+        void ComputeAge()
+        {
+            var computedAge = currentAge + AddedAge;
+
+            Debug.Log("Log: Computed Age: " + computedAge);
+            Debug.Log($"Log: Computed Age: {computedAge}");
+            Debug.LogFormat("LogFormat: Computed Age: {0}", computedAge);
+        }
+
+        public bool GenerateCharacter(string characterName)
+        {
+            Debug.Log($"{characterName} - Character generated");
+
+            return true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
